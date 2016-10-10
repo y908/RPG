3f2c547aba3fbb6b2ea2c383bf45c6b2351322ed
Yana Yakubchik <Yana Yakubchik>
2016-10-10 17:04:10 -0400

    first commit. added starter files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>RPG game</title>
+<!-- reset style  -->
+<link rel="stylesheet" type="text/css" href="reset.css">
+<!--main style -->
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<p>apple</p>
+
+
+</body>
+</html>
